50b8e5f003371b02593e45824eed89a24ed95ce2
Jonathane Leavell <jonathaneleavell@gmail.com>
2020-08-19 19:51:34 -0400

    finished assesment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 
 // This URL can be combined with an photo id to fetch an photo.
-const PHOTO_URL = photoId => `https://picsum.photos/id/${photoId}/200/200`;
+ const PHOTO_URL = (photoId) => `https://picsum.photos/id/${photoId}/200/200`;
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {};
+  state = { photos: [] };
+
+  async componentDidMount() {
+
+fetch(PHOTO_LIST_URL)
+.then(response => response.json())
+.then(response => {this.setState({photos: response})})
+
+
+    
+  }
 
   // 2. Declare a life cycle method
   // This life cycle method should:
@@ -23,21 +33,15 @@ class App extends Component {
         <header>
           <h1>Photo Wall</h1>
           <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and complete
-            the instructions there.
+            {console.log(photos)}
           </p>
         </header>
         <div className="collage">
           {/* We use map here because Array.prototype.map is an expression,
            * and for loops are not. You'll learn more about this soon!
            */}
-          {photos.map(photo => (
-            <img
-              alt={/* 3. Fill me in with the photo's filename */ ""}
-              key={/* 4. Fill me in with the photo's id */ ""}
-              src={/* 5. Fill me in with the photo's URL */ ""}
-            />
+          {photos.map((photo) => (
+            <img alt={photo.filename} key={photo.id} src={ `https://picsum.photos/id/${photo.id}/200/200`} />
           ))}
         </div>
       </React.Fragment>
