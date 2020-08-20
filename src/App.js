72bdb3cc9d7101ddb00065bd180148d8e555d65d
Jonathane Leavell <jonathaneleavell@gmail.com>
2020-08-19 20:02:07 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 // This URL can be combined with an photo id to fetch an photo.
- const PHOTO_URL = (photoId) => `https://picsum.photos/id/${photoId}/200/200`;
+const PHOTO_URL = (photoId) => `https://picsum.photos/id/${photoId}/200/200`;
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
@@ -11,14 +11,12 @@ class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
   state = { photos: [] };
 
-  async componentDidMount() {
-
-fetch(PHOTO_LIST_URL)
-.then(response => response.json())
-.then(response => {this.setState({photos: response})})
-
-
-    
+   componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ photos: response });
+      });
   }
 
   // 2. Declare a life cycle method
@@ -32,16 +30,17 @@ fetch(PHOTO_LIST_URL)
       <React.Fragment>
         <header>
           <h1>Photo Wall</h1>
-          <p>
-            {console.log(photos)}
-          </p>
         </header>
         <div className="collage">
           {/* We use map here because Array.prototype.map is an expression,
            * and for loops are not. You'll learn more about this soon!
            */}
           {photos.map((photo) => (
-            <img alt={photo.filename} key={photo.id} src={ `https://picsum.photos/id/${photo.id}/200/200`} />
+            <img
+              alt={photo.filename}
+              key={photo.id}
+              src={`https://picsum.photos/id/${photo.id}/200/200`}
+            />
           ))}
         </div>
       </React.Fragment>
